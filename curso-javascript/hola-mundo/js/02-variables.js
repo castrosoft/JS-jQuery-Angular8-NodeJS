b40b7c8864ccf6134b96efb7bfa7e61e2f15c646
Pedro Castro <castro.fcefyn@gmail.com>
2019-12-10 21:50:30 -0300

    Variables and comments

---
+++ curso-javascript/hola-mundo/js/02-variables.js
@@ -0,0 +1,7 @@
+var pais = 'Argentina';
+var continente = 'America';
+var antiguedad = 2019;
+var pais_y_continente = pais + ' ' + continente;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
