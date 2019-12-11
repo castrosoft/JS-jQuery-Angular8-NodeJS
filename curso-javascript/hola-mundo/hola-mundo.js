9a0099caffd787c5035791fbaad4c0fb5dac26ce
Pedro Castro <castro.fcefyn@gmail.com>
2019-12-10 21:13:10 -0300

    Hello world in JavaScript

---
+++ curso-javascript/hola-mundo/hola-mundo.js
@@ -0,0 +1,4 @@
+            alert("Hello world with JavaScript 1");
+            alert("Hello world with JavaScript 2");
+            alert("Hello world with JavaScript 3");
+            document.write("Hello from external file");
