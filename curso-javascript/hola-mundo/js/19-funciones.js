5d9d06d32b1192cc3f2d052d765325186964cb7a
Pedro Castro <castro.fcefyn@gmail.com>
2019-12-15 14:53:34 -0300

    Functions

---
+++ curso-javascript/hola-mundo/js/19-funciones.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function calculator(){
+    console.log(calculator);
+    console.log(calculator);
+    console.log(calculator);
+    console.log(calculator);    
+}
+
+calculator();
