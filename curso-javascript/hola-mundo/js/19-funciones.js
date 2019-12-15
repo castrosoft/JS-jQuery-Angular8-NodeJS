a27cef38e0e46f586a7c1c840656b602335e98a2
Pedro Castro <castro.fcefyn@gmail.com>
2019-12-15 15:30:54 -0300

    Functions inside other ones.

--- curso-javascript/hola-mundo/js/19-funciones.js
+++ curso-javascript/hola-mundo/js/19-funciones.js
@@ -3,17 +3,29 @@
 function calculator(number_1, number_2, toShow = true){
 
     if(toShow){
-        console.log("Sum: " + (number_1 + number_2));
-        console.log("Substraction: " + (number_1 - number_2));
-        console.log("Multiply: " + (number_1 * number_2));
-        console.log("Division: " + (number_1 / number_2));   
+        byConsole(number_1, number_2);
     }else{
-        document.write("Sum: " + (number_1 + number_2 + "<br/>"));
-        document.write("Substraction: " + (number_1 - number_2 + "<br/>"));
-        document.write("Multiply: " + (number_1 * number_2 + "<br/>"));
-        document.write("Division: " + (number_1 / number_2 + "<br/>"));
+        byDocument(number_1, number_2);
     }
+    return true;
 }
 
-//calculator(20, 5);
+
+function byConsole(number_1, number_2){
+    console.log("Sum: " + (number_1 + number_2));
+    console.log("Substraction: " + (number_1 - number_2));
+    console.log("Multiply: " + (number_1 * number_2));
+    console.log("Division: " + (number_1 / number_2));  
+}
+
+function byDocument(number_1, number_2){
+    document.write("Sum: " + (number_1 + number_2 + "<br/>"));
+    document.write("Substraction: " + (number_1 - number_2 + "<br/>"));
+    document.write("Multiply: " + (number_1 * number_2 + "<br/>"));
+    document.write("Division: " + (number_1 / number_2 + "<br/>"));    
+}
+
+
+
+calculator(20, 5);
 calculator(20, 5, false);
