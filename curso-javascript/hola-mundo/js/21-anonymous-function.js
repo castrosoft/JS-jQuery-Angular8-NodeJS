62f0fa84a18dc02e0edd820eb2b865322738ebd6
Pedro Castro <castro.fcefyn@gmail.com>
2019-12-15 17:21:08 -0300

    anonymous function and callbacks

---
+++ curso-javascript/hola-mundo/js/21-anonymous-function.js
@@ -0,0 +1,28 @@
+'use strict'
+
+//anonymous function 
+//It's a function without name
+
+var film = function(name){
+    return "The film is: " + name;
+}
+
+//callback: function inside other one.
+
+
+function sum(number1, number2, sumYShow, sumByTwo){
+    var sumar = number1 + number2;
+
+    sumYShow(sumar); //third parameter
+    sumByTwo(sumar); //fourth parameter
+
+    return sumar;
+}
+
+//call function with 4 parameters
+sum(5, 7, function(data){
+    console.log("The sum is: ", data);
+},
+function(data){
+    console.log("The sum by two is: ", (data * 2));
+});
