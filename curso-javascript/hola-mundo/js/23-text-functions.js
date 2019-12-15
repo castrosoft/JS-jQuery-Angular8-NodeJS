a97a8ec8603ea0f70cafa88f3beaa0847f0e9f55
Pedro Castro <castro.fcefyn@gmail.com>
2019-12-15 19:26:14 -0300

    replace functions

--- curso-javascript/hola-mundo/js/23-text-functions.js
+++ curso-javascript/hola-mundo/js/23-text-functions.js
@@ -5,25 +5,17 @@ var number = 10;
 var text1 = "Welcome to Mina Clavero Mina";
 var text2 =  " a great city";
 
-var searchWord = text1.indexOf("Mina");
-console.log(searchWord);
-var searchWord = text1.lastIndexOf("Mina");
-console.log(searchWord);
-var searchWord = text1.search("Mina"); //idem a indexOf
-console.log(searchWord);
-var searchWord = text1.match("Mina"); //return an array
-console.log(searchWord);
-var searchWord = text1.match(/Mina/g); //return an array. Global search
-console.log(searchWord);
-var searchWord = text1.substr(8,16); //part of sentence
-console.log(searchWord);
-var searchWord = text1.charAt(16); //just a letter
-console.log(searchWord);
-var searchWord = text1.startsWith("Wel"); //just search at begin of phrase
-console.log(searchWord);
-var searchWord = text1.endsWith("Mina");
-console.log(searchWord);
-var searchWord = text1.includes("Clavero");
-console.log(searchWord);
+var word = text1.replace("Mina Clavero", "Cura Brochero");
+console.log(word);
+var word = text1.slice(9);
+//var word = text1.slice(9,15);
+console.log(word);
+var word = text1.split(); //to put in array
+console.log(word);
+var word = text1.split(" "); 
+console.log(word);
+var word = text1.trim(); 
+console.log(word);
+
 
 
