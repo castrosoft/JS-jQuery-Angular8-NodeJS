e2d9cb523742aeee3eb00cfa27d5dab20708b423
Pedro Castro <castro.fcefyn@gmail.com>
2019-12-15 15:25:21 -0300

    Optionals Parameter

--- curso-javascript/hola-mundo/js/19-funciones.js
+++ curso-javascript/hola-mundo/js/19-funciones.js
@@ -1,10 +1,19 @@
 'use strict'
 
-function calculator(number_1, number_2){
-    console.log("Sum: " + (number_1 + number_2));
-    console.log("Substraction: " + (number_1 - number_2));
-    console.log("Multiply: " + (number_1 * number_2));
-    console.log("Division: " + (number_1 / number_2));   
+function calculator(number_1, number_2, toShow = true){
+
+    if(toShow){
+        console.log("Sum: " + (number_1 + number_2));
+        console.log("Substraction: " + (number_1 - number_2));
+        console.log("Multiply: " + (number_1 * number_2));
+        console.log("Division: " + (number_1 / number_2));   
+    }else{
+        document.write("Sum: " + (number_1 + number_2 + "<br/>"));
+        document.write("Substraction: " + (number_1 - number_2 + "<br/>"));
+        document.write("Multiply: " + (number_1 * number_2 + "<br/>"));
+        document.write("Division: " + (number_1 / number_2 + "<br/>"));
+    }
 }
 
-calculator(20, 5);
+//calculator(20, 5);
+calculator(20, 5, false);
